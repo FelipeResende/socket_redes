38bc5560fa11dd71a9f37166b97862f45b06a5c1
Felipe Cardoso Resende <felipecardoso.fcr@gmail.com>
2015-06-25 16:52:50 -0300

    Added threads in clients

--- client.c
+++ client.c
@@ -7,12 +7,16 @@
 #include "sock.h"
 #include "buffer.h"
 
-void *producer(void *);
-void *consumer(void *);
+void *producer();
+void *consumer();
+void *transmitter_handler();
 
 char texto[] = "Texto a ser escrito no buffer para testes...";
 int port;
+char file_name[200];
 FILE *fr;
+
+buffer b;
 
 #define check(A,msg) \
   if ((A)) { \
@@ -20,65 +24,80 @@ FILE *fr;
     exit(1); \
   }
     
+#define MAX_THREADS 10
 int main(int argc, char *argv[])
 {
-  buffer *b = malloc(sizeof(buffer));
   pthread_t pthread_consumidor, pthread_produtor;
 
-  if (argc != 2)
+  if (argc != 3)
   {
-    printf("Usage: %s port\n", argv[0]);
+    printf("Usage: %s port file\n", argv[0]);
     return 1;
   }
   port = atoi(argv[1]);
 
-  initBuffer(b);
+  strncpy(file_name, argv[2], 199);
+  file_name[199] = '\0';
 
-  check(pthread_create(&pthread_produtor, NULL, producer, (void *)b), "ERROR creating thread producer!!\n");
-  check(pthread_create(&pthread_consumidor, NULL, consumer, (void *)b), "ERROR creating thread consumer!!\n")
+  initBuffer(&b);
+
+  check(pthread_create(&pthread_produtor, NULL, producer, NULL), "ERROR creating thread producer!!\n");
+  check(pthread_create(&pthread_consumidor, NULL, consumer, NULL), "ERROR creating thread consumer!!\n")
   check(pthread_join(pthread_produtor, NULL), "ERROR joining thread producer")
   check(pthread_join(pthread_consumidor, NULL), "ERROR joining thread consumer")
 
-  destructBuffer(b);
+  destructBuffer(&b);
 
   pthread_exit(NULL);
   return 1;
 }
 
-void *producer(void * d)
+void *producer()
 {
   char c;
-  buffer *pb = (buffer *)d;
-  fr = fopen ("disco", "rt");
+  buffer *pb = &b;
+  fr = fopen (file_name, "rb");
   c = fgetc(fr);
   while( c != EOF )
   {
-    //printf("Producer: %c\n", ch);
     pb->insert(pb, c);
     c = fgetc(fr); 
   }
-  pb->insert(pb, c);
   fclose(fr);  /* close the file prior to exiting the routine */
   return 0;
 }
-void *consumer(void *d)
+void *consumer()
 {
-  buffer *pb = (buffer *)d;
+  pthread_t n_threads[MAX_THREADS];
+  for (int i = 0; i < MAX_THREADS; i++)
+  {
+    printf("thrd: %d\n", i);
+    check(pthread_create(&n_threads[i], NULL, transmitter_handler, NULL), "Failed to create the client consumer thread");
+  }
+  for (int i = 0; i < MAX_THREADS; i++)
+  {
+    check(pthread_join(n_threads[i], NULL), "Error joining threads");
+  }
+
+  return NULL;
+}
+
+void *transmitter_handler()
+{
+  buffer *pb = &b;
   int s = sock_conn("localhost", port);
   if (s < 0)
     printf("Erro: s menor que zero!\n");
 
-  printf("2\n");
   char c = 'c';
-  while (1 && c != EOF)
+  while (c != EOF)
   {
     c = pb->get(pb);
-    /*printf("\t\tConsumer: %c\n", c);*/
     if (send(s, &c, 1, 0) < 0)
     {
       puts("Send failed\n");
     }
   }
-  close(s);
-  return 0;
+  /*close(s);*/
+  return NULL;
 }
