111f36f465e58925c3f795d79a825660228749ad
Felipe Cardoso Resende <felipecardoso.fcr@gmail.com>
2015-06-25 11:23:25 -0300

    Added multiple threads to receive the data via sockets.

--- server.c
+++ server.c
@@ -12,14 +12,18 @@
     exit(1); \
   }
 
-void *producer(void *);
-void *consumer(void *);
+#define MAX_THREADS 10
+void *producer();
+void *consumer();
+void *receiver_handler(void *n);
 
 int port;
 
+buffer b;
+
 int main(int argc, char *argv[])
 {
-  buffer *b = malloc(sizeof(buffer));
+  /*buffer *b = malloc(sizeof(buffer));*/
   pthread_t pthread_consumidor, pthread_produtor;
 
   if (argc != 2)
@@ -29,41 +33,59 @@ int main(int argc, char *argv[])
   }
   port = atoi(argv[1]);
 
-  initBuffer(b);
+  initBuffer(&b);
 
-  check(pthread_create(&pthread_produtor, NULL, producer, (void *)b), "ERROR creating thread producer!!\n");
-  check(pthread_create(&pthread_consumidor, NULL, consumer, (void *)b), "ERROR creating thread consumer!!\n")
+  check(pthread_create(&pthread_produtor, NULL, producer, NULL), "ERROR creating thread producer!!\n");
+  check(pthread_create(&pthread_consumidor, NULL, consumer, NULL), "ERROR creating thread consumer!!\n")
   check(pthread_join(pthread_produtor, NULL), "ERROR joining thread producer")
   check(pthread_join(pthread_consumidor, NULL), "ERROR joining thread consumer")
 
-  destructBuffer(b);
+  destructBuffer(&b);
 
-  pthread_exit(NULL);
   return 1;
 }
 
-void *producer(void *b)
+void *producer()
 {
-  buffer *pb = (buffer *)b;
-  int n = sock_bind(port, 3);
-  int n_client = sock_accept(n);
-  int read_size;
-  char m;
+  int n = sock_bind(port, MAX_THREADS);
+  /*int n_client = sock_accept(n);*/
+  int n_clients[MAX_THREADS];
+  pthread_t n_threads[MAX_THREADS];
 
-  if (n < 0 || n_client < 0)
-    pthread_exit(NULL);
-
-  while ( (read_size = recv(n_client, &m, 1, 0)) > 0 )
+  for (int i = 0; i < MAX_THREADS; i++)
   {
-    /*printf("Server producer: %c\n", m);*/
-    pb->insert(pb, m);
+    n_clients[i] = sock_accept(n);
+    if (n < 0 || n_clients[i] < 0)
+      pthread_exit(NULL);
+
+    check(pthread_create(&n_threads[i], NULL, receiver_handler, (void *)&n_clients[i]), "Nao foi possivel criar a thread.\n");
+    printf("thrd %d", i);
   }
+
+  for (int i = 0; i < MAX_THREADS; i++) check(pthread_join(n_threads[i], NULL), "Error"); 
+
   return NULL;
 }
 
-void *consumer(void *b)
+void *receiver_handler(void *n)
 {
-  buffer *pb = (buffer *)b;
+  static int count = 0;
+  int n_client = *(int *)n;
+  int read_size;
+  char m;
+  buffer *pb = &b;
+
+  printf("Thread %d\n", count++);
+  while ( (read_size = recv(n_client, &m, 1, 0)) > 0 )
+  {
+    pb->insert(pb, m);
+  }
+
+  return NULL;
+}
+void *consumer()
+{
+  buffer *pb = &b;
   while (1)
   {
     char c = pb->get(pb);
