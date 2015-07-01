4b5256656606d2dd54343720a6cfcabc04969d3a
Felipe Cardoso Resende <felipecardoso.fcr@gmail.com>
2015-06-30 22:35:09 -0300

    Number os threads is passed by command line, now the program ends when the transmission finishes. Also included time measurements.

--- client.c
+++ client.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
+#include <unistd.h>
+#include <time.h>
 
 #include <sys/socket.h>
 #include "sock.h"
@@ -11,7 +13,6 @@ void *producer();
 void *consumer();
 void *transmitter_handler();
 
-char texto[] = "Texto a ser escrito no buffer para testes...";
 int port;
 char file_name[200];
 FILE *fr;
@@ -23,14 +24,14 @@ buffer b;
     exit(1); \
   }
     
-#define MAX_THREADS 2
+int MAX_THREADS;
 int main(int argc, char *argv[])
 {
   pthread_t pthread_consumidor, pthread_produtor;
 
-  if (argc != 3)
+  if (argc != 4)
   {
-    printf("Usage: %s port file\n", argv[0]);
+    printf("Usage: %s port filename nthreads\n", argv[0]);
     return 1;
   }
   port = atoi(argv[1]);
@@ -38,13 +39,17 @@ int main(int argc, char *argv[])
   strncpy(file_name, argv[2], 199);
   file_name[199] = '\0';
 
+  MAX_THREADS = atoi(argv[3]);
   initBuffer(&b);
 
+  double start = clock();
   check(pthread_create(&pthread_produtor, NULL, producer, NULL), "ERROR creating thread producer!!\n");
   check(pthread_create(&pthread_consumidor, NULL, consumer, NULL), "ERROR creating thread consumer!!\n")
-  check(pthread_join(pthread_produtor, NULL), "ERROR joining thread producer")
   check(pthread_join(pthread_consumidor, NULL), "ERROR joining thread consumer")
+  check(pthread_join(pthread_produtor, NULL), "ERROR joining thread producer")
 
+  double end = clock();
+  printf("Tempo total de execucao: %.2e\n", ((double)(end - start)/ CLOCKS_PER_SEC));
   destructBuffer(&b);
   pthread_exit(NULL);
   return 1;
@@ -61,7 +66,11 @@ void *producer()
   while( elem.c != EOF )
   {
     elem.pos = ftell(fr);
-    elem.c = fgetc(fr); 
+    elem.c = fgetc(fr);
+    pb->insert(pb, elem);
+  }
+  for (int i = 0; i < MAX_THREADS; i++)
+  {
     pb->insert(pb, elem);
   }
   fclose(fr);  /* close the file prior to exiting the routine */
@@ -72,7 +81,6 @@ void *consumer()
   pthread_t n_threads[MAX_THREADS];
   for (int i = 0; i < MAX_THREADS; i++)
   {
-    printf("thrd: %d\n", i);
     check(pthread_create(&n_threads[i], NULL, transmitter_handler, NULL), "Failed to create the client consumer thread");
   }
   for (int i = 0; i < MAX_THREADS; i++)
@@ -94,11 +102,14 @@ void *transmitter_handler()
   elem  = pb->get(pb);
   while (elem.c != EOF)
   {
+    // Iniciar medicao de tempo aqui
     if (send(s, &elem, sizeof(elem), 0) < 0)
     {
       puts("Send failed\n");
     }
-    elem  = pb->get(pb);
+    // Terminar medicao de tempo aqui
+    elem = pb->get(pb);
   }
+  close(s);
   return NULL;
 }
